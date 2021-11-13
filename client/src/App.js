7936ecbe5e8d6ac9e7a5b08954370784b68c097c
PawanGulati <undefended0@gmail.com>
2021-11-13 11:46:06 +0530

    404 added

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import {Switch, Route, useLocation, Redirect} from 'react-router-dom'
 
-import HomePage from './views/containers/HomePage';
+import HomePage from './views/containers/LandingPage';
 import NotFound from './views/containers/NotFound';
 import Layout from './views/containers/Layout';
 import StudentHomeView from './views/components/student-views/StudentHomeView';
@@ -10,18 +10,24 @@ import StudentHomeView from './views/components/student-views/StudentHomeView';
 function App(props) {
   let {pathname} = useLocation()
 
-  let user = null;
+  let user = true;
 
   return (
     <div className="App">
-      <Layout user={user}>
         <Switch>
           <Redirect from="/:url*(/+)" to={pathname.slice(0, -1)} />
-          <Route path='/' exact component={user ? StudentHomeView : HomePage} />
+          <Route path='/' exact render={
+            ()=> 
+              <Layout user={user}>
+                {
+                  user ? <StudentHomeView/> : <HomePage />
+                }
+              </Layout>
+            }
+          />
           <Route path='/404' component={NotFound} />
           <Redirect to='/404' />
         </Switch>
-      </Layout>
     </div>
   );
 }
--- client/src/views/containers/NotFound.jsx
+++ client/src/views/containers/NotFound.jsx
@@ -1,9 +1,58 @@
+import { Button, Typography } from '@mui/material'
 import React from 'react'
+import {makeStyles} from '@mui/styles'
 
-export default function NotFound() {
+import {ReactComponent as NotFoundIMG} from '../../constants/images/404.svg'
+
+const useStyles = makeStyles(theme=>({
+    root:{
+        width:'100%',
+        height:'100%',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    image:{
+        height:'45rem',
+        width:'100%'
+    },
+    textPane:{
+        display:'flex',
+        textAlign:'left',
+        alignItems:'center',
+        flexDirection:'column',
+        height:'100%',
+        width:'70%',
+        position:'relative'
+    },
+    headlineTypo:{
+        width:'25rem',
+        fontWeight:900,
+        fontSize:'4.3rem',
+        margin:'1rem'
+    },
+    paraTypo:{  
+        width:'25rem',
+        height:'100%',
+    },
+    button:{
+        borderRadius:10,
+        margin:'4rem 0',
+        marginRight:'43%',
+    }
+}))
+
+export default function NotFound({history}) {
+    const classes = useStyles()
+
     return (
-        <div>
-            
+        <div className={classes.root}>
+            <div className={classes.textPane}>
+                <Typography variant='h2' className={classes.headlineTypo}>I have bad news for you</Typography>
+                <Typography variant='h6' className={classes.paraTypo}>The page you are looking for might be removed or is temporarily unavailable</Typography>
+                <Button size='large' className={classes.button} variant='contained' color='primary' onClick={()=>{history.push('/')}}>Come Home</Button>
+            </div>
+            <NotFoundIMG className={classes.image} />
         </div>
     )
 }
