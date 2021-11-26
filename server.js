67f84b85bbce3d45b422f75de058e80cf566f2a1
PawanGulati <undefended0@gmail.com>
2021-11-26 18:53:36 +0530

    silly mistake

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const errorControl = require('./controllers')
 const routes = require('./routes')
