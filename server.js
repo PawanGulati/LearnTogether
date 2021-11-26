69dd51aec37f949187be0020ea416e6b883828b6
PawanGulati <undefended0@gmail.com>
2021-11-26 18:59:50 +0530

    path correction

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(express.json())
 
 // production static files
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"../client/build")))
+    app.use(express.static(path.join(__dirname,"client/build")))
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../client/build","index.html"))
+        res.sendFile(path.join(__dirname,"client/build","index.html"))
     })
 }
 
